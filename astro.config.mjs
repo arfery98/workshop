e4ef3b91faac22a4ae263bd8fef541886ce5bcf0
AFERY_RS <arferyrs@gmail.com>
2025-05-19 23:44:23 +0700

    ครั้งที่2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [tailwindcss()],   
     },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{astro,vue,,html,md,mdx,svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true ,
+        padding: "1rem",
+        /* screen: {
+          'sm': "640px",
+          'md': "768px",
+          'lg': "1024px",
+          'xl': "1024px",
+          '2xl': "1024px", 
+        }*/
+      }
+    },
+  },
+  plugins: [],
+}
