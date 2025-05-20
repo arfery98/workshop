4ebe3acd50fefcb9688bd525091ddd04f8fe21cf
AFERY_RS <arferyrs@gmail.com>
2025-05-20 23:21:35 +0700

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,11 @@ export default {
         center: true ,
         padding: "1rem",
         screen: {
-          'none': "100%",
           'sm': "640px",
           'md': "768px",
           'lg': "1024px",
-          'xl': "1280px",
-          '2xl': "1536px", 
+          'xl': "1024px",
+          '2xl': "1024px", 
         }
       }
     },
