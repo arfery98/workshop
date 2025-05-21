ac5036ff20c7c5a683a5ec9a04b2a535283189db
AFERY_RS <arferyrs@gmail.com>
2025-05-21 17:28:38 +0700

    responsive v.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,20 @@ export default {
   theme: {
     extend: {
       container: {
-        center: true ,
+        center: true,
         padding: "1rem",
         screen: {
           'sm': "640px",
           'md': "768px",
           'lg': "1024px",
           'xl': "1024px",
-          '2xl': "1024px", 
+          '2xl': "1024px",
         }
       }
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
 }
